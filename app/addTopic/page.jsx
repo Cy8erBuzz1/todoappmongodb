6de610d7535669f6852f4c6f53e0318263418fce
Cy8erBuzz1 <opitz88@proton.me>
2024-11-11 12:23:15 +0100

    todoapp postman stage

---
+++ app/addTopic/page.jsx
@@ -0,0 +1,24 @@
+export default function AddTopic (){
+    return ( 
+    
+    <form className="flex flex-col gap-3">
+
+        <input className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Topic Titel"               
+        />
+
+
+        <input className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Topic Description"
+        />
+
+        <button className="bg-green-600 font-bold
+        text-white py-6 px-6 w-fit">
+            Add Topic
+        </button>
+
+    </form>
+    );
+}
